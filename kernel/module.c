098781d03ca29945c6780d9569ded7ed42d92b37
Alfred Neumayer <dev.beidl@gmail.com>
2024-12-08 06:52:59 +0100

    kernel: module: Hack to circumvent CRC check

--- kernel/module.c
+++ kernel/module.c
@@ -1309,6 +1309,9 @@ static int check_version(const struct load_info *info,
 	unsigned int i, num_versions;
 	struct modversion_info *versions;
 
+        // HALIUM: Allow despite CRC mismatch
+        return 1;
+
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
 	if (!crc)
 		return 1;
