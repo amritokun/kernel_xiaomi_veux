e1f8cef7bf7e086e41f9b5baf028758ce06d4be6
Muhammad <thevancedgamer@mentallysanemainliners.org>
2025-04-29 10:42:14 +0500

    qcacld: load the driver by default
    Signed-off-by: Muhammad <thevancedgamer@mentallysanemainliners.org>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -241,10 +241,12 @@ int wlan_start_ret_val;
 static DECLARE_COMPLETION(wlan_start_comp);
 static qdf_atomic_t wlan_hdd_state_fops_ref;
 #ifdef MODULE
+#if 0
 static unsigned int dev_num = 1;
 static struct cdev wlan_hdd_state_cdev;
 static struct class *class;
 static dev_t device;
+#endif
 #endif
 static bool hdd_loaded = false;
 
@@ -16979,6 +16981,7 @@ const struct file_operations wlan_hdd_state_fops = {
 
 #ifdef MODULE
 #ifndef FEATURE_WLAN_RESIDENT_DRIVER
+#if 0
 static int  wlan_hdd_state_ctrl_param_create(void)
 {
 	unsigned int wlan_hdd_state_major = 0;
@@ -17043,6 +17046,7 @@ static void wlan_hdd_state_ctrl_param_destroy(void)
 
 	pr_info("Device node unregistered");
 }
+#endif
 #endif
 #endif
 
@@ -17915,7 +17919,9 @@ void hdd_driver_unload(void)
 	pld_deinit();
 #ifdef MODULE
 #ifndef FEATURE_WLAN_RESIDENT_DRIVER
+#if 0
 	wlan_hdd_state_ctrl_param_destroy();
+#endif
 #endif
 #endif
 	hdd_set_conparam(0);
@@ -17952,7 +17958,7 @@ static int hdd_module_init(void)
 {
 	int ret;
 
-	ret = wlan_hdd_state_ctrl_param_create();
+	ret = hdd_driver_load();
 	if (ret)
 		pr_err("wlan_hdd_state_create:%x\n", ret);
 
